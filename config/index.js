9eb2eace1303d05dceef4d4bd55b82691ee35929
Drake <83844803+drakemods@users.noreply.github.com>
2024-10-08 13:10:05 -0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,8 +27,8 @@ module.exports = {
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
-        clientId: process.env.RPP_DISCORD_CLIENT_ID || '',
-        token: process.env.RPP_DISCORD_TOKEN || '',
+        clientId: process.env.RPP_DISCORD_CLIENT_ID || '1292964950641479720',
+        token: process.env.RPP_DISCORD_TOKEN || 'MTI5Mjk2NDk1MDY0MTQ3OTcyMA.Gq7OUk.AeTS2gJEOxrbUcsAaisBmfKyLW89DqkkE6wa08',
         needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true, /* If true, only admins can delete (server, switch..), manage credentials and reset a channel */
     }
 };
